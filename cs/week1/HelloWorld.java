fbcc5057b8d335604a8db59e61ec4e0f6294d98e
Lloyd Dilley <ldilley@users.noreply.github.com>
2023-11-26 16:30:04 -0500

    Add HelloWorld.java

---
+++ cs/week1/HelloWorld.java
@@ -0,0 +1,12 @@
+/*
+ * Coursera/Princeton University Computer Science: Programming with a Purpose week 1 assignment
+ * Lloyd Dilley
+ */
+
+class HelloWorld
+{
+  public static void main(String[] args)
+  {
+    System.out.println("Hello, World");
+  }
+}
