2be675d158e2116f28f2e3c3a6a5d14795afe87e
Lloyd Dilley <ldilley@users.noreply.github.com>
2023-12-01 21:24:17 -0500

    Add RightTriangle.java

---
+++ cs/week1/RightTriangle.java
@@ -0,0 +1,51 @@
+/*
+ * Coursera/Princeton University Computer Science: Programming with a Purpose
+ * Week 1: RightTriangle.java
+ * Lloyd Dilley
+ */
+
+class RightTriangle
+{
+  public static boolean isRight(int x, int y, int z)
+  {
+    return x * x + y * y == z * z;
+  }
+
+  public static void main(String[] args)
+  {
+    int a, b, c;
+    boolean retval;
+
+    if(args.length < 3)
+    {
+      System.err.println("This program requires 3 arguments!");
+      System.exit(1);
+    }
+
+    try
+    {
+      a = Integer.parseInt(args[0]);
+      b = Integer.parseInt(args[1]);
+      c = Integer.parseInt(args[2]);
+      /*if(a < 0 || b < 0 || c < 0)
+      {
+        System.err.println("All arguments must be positive integers!");
+        System.exit(1);
+      }*/
+
+      if(a < b && b < c)
+        retval = isRight(a, b, c);
+      else if(a < b && c < b)
+        retval = isRight(a, c, b);
+      else
+        retval = isRight(b, c, a);
+
+      System.out.println(retval);
+    }
+    catch(NumberFormatException nfe)
+    {
+      System.err.println("All arguments must be integers!");
+      System.exit(1);
+    }
+  }
+}
