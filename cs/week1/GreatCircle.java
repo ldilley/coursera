b902a73e6396c078708f482c26e999c3d451117c
Lloyd Dilley <ldilley@users.noreply.github.com>
2023-12-01 21:24:55 -0500

    Add GreatCircle.java

---
+++ cs/week1/GreatCircle.java
@@ -0,0 +1,47 @@
+/*
+ * Coursera/Princeton University Computer Science: Programming with a Purpose
+ * Week 1: GreatCircle.java
+ * Lloyd Dilley
+ */
+
+class GreatCircle
+{
+  final static double EARTH_RADIUS = 6371.0;
+
+  public static double calcDist(double x1, double y1, double x2, double y2)
+  {
+    return 2 * EARTH_RADIUS * Math.asin(Math.sqrt((Math.pow(Math.sin((x2 - x1) / 2.0), 2) + Math.cos(x1) * Math.cos(x2) * Math.pow(Math.sin((y2 - y1) / 2.0), 2))));
+  }
+
+  public static void main(String[] args)
+  {
+    double x1, y1, x2, y2;
+    boolean distance;
+
+    if(args.length < 4)
+    {
+      System.err.println("This program requires 4 arguments!");
+      System.exit(1);
+    }
+
+    try
+    {
+      x1 = Math.toRadians(Double.parseDouble(args[0]));
+      y1 = Math.toRadians(Double.parseDouble(args[1]));
+      x2 = Math.toRadians(Double.parseDouble(args[2]));
+      y2 = Math.toRadians(Double.parseDouble(args[3]));
+      /*if(x1 < 0 || y1 < 0 || x2 < 0 || y2 < 0)
+      {
+        System.err.println("All arguments must be positive doubles!");
+        System.exit(1);
+      }*/
+
+      System.out.println(calcDist(x1, y1, x2, y2) + " kilometers");
+    }
+    catch(NumberFormatException nfe)
+    {
+      System.err.println("All arguments must be doubles!");
+      System.exit(1);
+    }
+  }
+}
